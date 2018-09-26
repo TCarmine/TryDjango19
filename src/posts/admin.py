0316b9bc0c63a7a68dbe03e80b6f7e2a44d9cd7b
Carmine <kappa85t@hotmail.it>
2018-09-27 00:36:30 +0200

    fixed,filter in admin,added  list_filter in app model

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+#relative import
+
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+    #check docs
+    list_display = ["title","updated","timestamp"]
+    list_display_links = ["updated"]
+    list_filter=["updated","title"]
+    #only those in list_display
+    list_editable = ["title"]
+    #list_display_filter = ["updated","timestamp"]
+
+    search_fields = ["title","content"]
+    class Meta:
+        model = Post
+
+admin.site.register(Post,PostModelAdmin)
--- src/posts/models.py
+++ src/posts/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField (auto_now=False, auto_now_add=True)
 
-
+    #Python 2.7
     def __unicode__(self):
         return self.title
+    #Python 3.4
+    #def __str__(self):
+    #   return self.title
