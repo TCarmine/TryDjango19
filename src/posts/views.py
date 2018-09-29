c519672d842ce857e91e2bd2ada5b3242d9dc8eb
Carmine <kappa85t@hotmail.it>
2018-09-29 18:23:02 +0200

    added some queryset in template, index.html

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,17 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
 def  post_create(request):
 
     return HttpResponse("<h1>Create</h1>")
 
 def  post_list(request):
-
-    return HttpResponse("<h1>List</h1>")
+    queryset = Post.objects.all()
+    context={
+         "object_list":queryset,
+               "title":"List"
+    }
+    # if request.user.is_authenticated():
+    #         context={
+    #                 "title":"My user List"
+    #         }
+    # else:
+    #     context={
+    #     "title":"List"
+    #     }
+    return render(request,"index.html",context)
 
 def  post_detail(request):
-    return HttpResponse("<h1>detail</h1>")
+    context={
+    "title":"Detail"
+    }
+
+    return render(request,"index.html",context)
 
 
 def  post_update(request):
