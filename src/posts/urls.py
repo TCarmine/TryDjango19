f8cfb9632f6e71c897d3ba5a10a9a9753aec5769
Carmine <kappa85t@hotmail.it>
2018-09-30 11:49:12 +0200

    added dinamic url ref in template and explicit import vies

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,14 +1,20 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
+from .views import (
+post_list,
+post_create,
+post_detail,
+post_update,
+post_delete,
+)
 
 # ^ beginning string,$ end string, so a URL string need to be
 # exactly matched if between those 2
 urlpatterns = [
     url(r'^$',        "posts.views.post_list"),
     url(r'^create/$',  "posts.views.post_create"),
-    url(r'^(?P<id>\d+)$',  "posts.views.post_detail"),
+    url(r'^(?P<id>\d+)$', post_detail, name='detail'),
     url(r'^update/$', "posts.views.post_update"),
     url(r'^delete/$', "posts.views.post_delete"),
 ]
